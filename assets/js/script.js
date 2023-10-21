24868dd8c0539d7b391c38977ff9f8aa07109bc6
Sonia Majewska <sonianltran@gmail.com>
2023-10-21 14:20:21 +0000

    Added DOMContentLoaded event to js script and beautified the codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,43 +8,44 @@ document.addEventListener("DOMContentLoaded", function () {
     var currentQuestionIndex = 0;
     var gameIsOver = true;
     var timerInterval;
-    
-    /* Function to play sound */
+
+    // Function to play sound
     function playSound(audioURL) {
-        if (canPlaySound === true) {
+        if(canPlaySound === true) {
             var audio = new Audio(audioURL);
             audio.play();
         }
     }
-    
-    /* Function to show an element by changing its display style to 'block' */
+
+    // Function to show an element by changing its display style to 'block'
     function show(elementId) {
         var element = document.getElementById(elementId);
-        if (element) {
+        if(element) {
             element.style.display = 'block';
         }
     }
-    
-    /* Function to hide an element by changing its display style to 'none' */
+
+    // Function to hide an element by changing its display style to 'none'
     function hide(elementId) {
         var element = document.getElementById(elementId);
-        if (element) {
+        if(element) {
             element.style.display = 'none';
         }
     }
-    
-    // Wrap code in a event listener to Start/Reset button
-    document.getElementById('startReset').addEventListener('click', function() {
-        if (!gameIsOver) {
+
+    // Get the Start/Reset button element and add an event listener
+    var startResetButton = document.getElementById('startReset');
+    startResetButton.addEventListener('click', function () {
+        if(!gameIsOver) {
             var shouldReset = confirm("Are you sure you want to reset the game?");
-            if (shouldReset) {
+            if(shouldReset) {
                 resetGame();
             }
         } else {
             startGame();
         }
     });
-    
+
     /* Function to reset the game */
     function resetGame() {
         gameIsOver = true;
@@ -64,25 +65,22 @@ document.addEventListener("DOMContentLoaded", function () {
         setAnswerButtonsEnabled(false);
         questions.length = 0;
     }
-    
+
     /* Function to start/update/stop countdown timer */
     function startCountdown() {
         var startTime = new Date().getTime();
         var interval = 1000; // Set the interval to 1000ms (1 second)
-        
         function updateTimer() {
             var currentTime = new Date().getTime();
             var elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
             var remainingSeconds = timeremaining - elapsedSeconds;
-            
-            if (remainingSeconds >= 0) { 
+            if(remainingSeconds >= 0) {
                 document.getElementById("timeremainingvalue").innerHTML = remainingSeconds;
             } else {
                 stopCountdown();
-                if (!gameIsOver) {
+                if(!gameIsOver) {
                     show("gameOver");
                     document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";
-                    
                     hide("timeremaining");
                     hide("correct");
                     hide("incorrect");
@@ -91,17 +89,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("startReset").innerHTML = "START GAME";
             }
         }
-        
         timerInterval = setInterval(updateTimer, interval);
     }
-    
+
     function stopCountdown() {
-        clearInterval(timerInterval); 
+        clearInterval(timerInterval);
     }
-    
+
     /* Function to start the game */
     function startGame() {
-        if (gameIsOver) {
+        if(gameIsOver) {
             gameIsOver = false;
             currentQuestionIndex = 0;
             score = 0;
@@ -115,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("startReset").innerHTML = "RESET GAME";
             stopCountdown();
             questions.length = 0;
-            for (var i = 0; i < 10; i++) {
+            for(var i = 0; i < 10; i++) {
                 questions.push(generateQuestion());
             }
             displayQuestion();
@@ -123,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
             setAnswerButtonsEnabled(true); // Enable the answer buttons when the game starts
         }
     }
-    
+
     /* Function to generate random addition and subtraction questions */
     function generateQuestion() {
         var minNumber = 1;
@@ -132,65 +129,59 @@ document.addEventListener("DOMContentLoaded", function () {
         var operator = Math.random() < 0.5 ? '+' : '-';
         var answer;
         var num2 = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-        
-        if (operator === '+') {
+        if(operator === '+') {
             answer = num1 + num2;
         } else {
             // Ensure there are no negative answers
-            if (num1 < num2) {
+            if(num1 < num2) {
                 var temp = num1;
                 num1 = num2;
                 num2 = temp;
             }
             answer = num1 - num2;
         }
-        
         var questionText = num1 + ' ' + operator + ' ' + num2;
         // Ensure the answer is a positive integer
-        if (answer < 0) {
+        if(answer < 0) {
             return generateQuestion();
         }
-        
         return {
             text: questionText,
             answer: answer
         };
     }
-    
+
     /* Display a question and answer choices */
     function displayQuestion() {
         var question = questions[currentQuestionIndex];
         document.getElementById('question-area').textContent = question.text;
         var answers = [];
-        
         // Generate a list of unique answer choices including the correct answer
         answers.push(question.answer);
-        while (answers.length < 4) {
+        while(answers.length < 4) {
             var randomAnswer = Math.floor(Math.random() * 40);
-            if (!answers.includes(randomAnswer)) {
+            if(!answers.includes(randomAnswer)) {
                 answers.push(randomAnswer);
             }
         }
-        
         // Shuffle the answer choices for a random order
         answers = shuffleArray(answers);
-        
+
         function createClickHandler(answer) {
-             return function () {
+            return function () {
                 checkAnswer(answer);
             };
         }
-        
-        for (var i = 0; i < 4; i++) {
+        for(var i = 0; i < 4; i++) {
             var box = document.getElementById('box' + (i + 1));
             box.textContent = answers[i];
             box.onclick = createClickHandler(answers[i]);
         }
     }
-    
+
     /* Function to shuffle an array (Fisher-Yates shuffle algorithm */
     function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
+        for(var i = array.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
             var temp = array[i];
             array[i] = array[j];
@@ -198,18 +189,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         return array;
     }
-    
+
     /* Check if the selected answer is correct */
     function checkAnswer(selectedAnswer) {
         var question = questions[currentQuestionIndex];
-        if (!gameIsOver) {
-            if (selectedAnswer === question.answer) {
+        if(!gameIsOver) {
+            if(selectedAnswer === question.answer) {
                 // Correct answer: Increase the score
                 score++;
                 document.getElementById("scoreValue").innerHTML = score; // Update the displayed score
                 currentQuestionIndex++;
                 playSound("assets/sounds/meow.mp3");
-                if (currentQuestionIndex < questions.length) {
+                if(currentQuestionIndex < questions.length) {
                     displayQuestion(); // Display the next question
                 } else {
                     endGame();
@@ -220,30 +211,26 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     }
-    
+
     /* Function to display correct or try again message */
     function displayCorrect() {
         var correctDiv = document.getElementById('correct');
         correctDiv.style.display = 'block'; // Make the "Correct!" message visible
-        
         /* Use a named function for the setTimeout callback */
         function hideCorrectDiv() {
             correctDiv.style.display = 'none'; // Hide the message after a certain time (e.g., 1 second)
         }
-        
         setTimeout(hideCorrectDiv, 1000);
     }
-    
+
     function displayIncorrect() {
         var incorrectDiv = document.getElementById('incorrect');
         incorrectDiv.style.display = 'block';
         playSound("assets/sounds/no_meow.mp3");
-        
         /* Use a named function for the setTimeout callback */
         function hideIncorrectDiv() {
             incorrectDiv.style.display = 'none';
         }
-        
         setTimeout(hideIncorrectDiv, 1000);
     }
 
@@ -254,27 +241,24 @@ document.addEventListener("DOMContentLoaded", function () {
         gameOverDiv.style.display = 'block';
         setAnswerButtonsEnabled(false); // Disable the answer buttons when the game ends
     }
-    
     /* Function to set the answer buttons' enabled/disabled state */
     function setAnswerButtonsEnabled(enabled) {
         var answerButtons = document.getElementsByClassName('answer-button');
-        for (var i = 0; i < answerButtons.length; i++) {
+        for(var i = 0; i < answerButtons.length; i++) {
             answerButtons[i].style.pointerEvents = enabled ? 'auto' : 'none';
         }
     }
-    
+
     /* Define a click event handler function for the Start/Reset button */
     function handleClick() {
-    startGame();
-    document.getElementById('gameOver').style.display = 'none';
+        startGame();
+        document.getElementById('gameOver').style.display = 'none';
     }
-        
+    
     // Add the click event listener using the handler function
-    document.getElementById('startReset').addEventListener('click', function() {
-        if (!playing) {
+    document.getElementById('startReset').addEventListener('click', function () {
+        if(!playing) {
             startGame();
         }
     });
 });
-
-
